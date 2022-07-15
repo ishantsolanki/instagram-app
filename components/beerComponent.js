6a943832f4cecc6f685a993fe85824b6f8130f84
Ishant Solanki <ishant360@gmail.com>
2022-07-15 21:54:19 +0100

    Enlarge product image to take max available width

--- components/beerComponent.js
+++ components/beerComponent.js
@@ -1,18 +1,20 @@
-import { beerDisplay, beerImage } from "./beerComponent.module.css";
+import { beerDisplay, beerImage, imageContainer } from "./beerComponent.module.css";
 
 const BeerComponent = ({ beer }) => {
-  return (
-    <div className={beerDisplay}>
-      <img
-        className={beerImage}
-        src={beer.image_url}
-        alt={beer.name}
-        height={100}
-      />
-      <div>{beer.name}</div>
-      <button>Buy</button>
-    </div>
-  );
+    return (
+        <div className={beerDisplay}>
+            <div className={imageContainer}>
+                <img
+                    className={beerImage}
+                    src={beer.image_url}
+                    alt={beer.name}
+                    height="100%"
+                />
+            </div>
+            <div>{beer.name}</div>
+            <button>Buy</button>
+        </div>
+    );
 };
 
 export default BeerComponent;
