4e2ecf46ef2d6c0383967871373c3f94567b13f3
Ishant Solanki <ishant360@gmail.com>
2022-07-17 11:09:40 +0100

    Add offline support

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
+const withPWA = require('next-pwa')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  pwa: {
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+  },
 }
 
-module.exports = nextConfig
+module.exports = withPWA(nextConfig)
